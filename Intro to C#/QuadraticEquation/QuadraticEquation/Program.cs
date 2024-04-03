25cbf988de88d0677d4362ec228f1c6f1b011d83
Abdo <abdomoon100@gmail.com>
2024-04-03 17:26:53 +0200

    Added new 3 Projects

---
+++ Intro to C#/QuadraticEquation/QuadraticEquation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Factorial
+{
+    static void Main()
+    {
+        Console.WriteLine("Write N Then Write K");
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        int facN = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            facN *= i;
+        };
+        int facK = 1;
+        for (int i = 1; i <= k; i++)
+        {
+            facK *= i;
+        }
+        Console.WriteLine($"The Result for  N! / K! is :{facN/facK}");
+    }
+}
---
+++ Intro to C#/The value of Z/The value of Z/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ValueOfZ
+{
+    static void Main()
+    {
+        // the wanted Z
+        int z = 0;
+        // the j and i to use in loops 
+        int j = 0;
+        int i = 0;
+
+        while (i < 6)
+        {
+            j = 0;
+            while (j <= 4)
+            {
+                z += i * j;
+                j++;
+            }
+            i++;
+        } ;
+        Console.WriteLine($"Z equals : {z}");
+    }
+}
