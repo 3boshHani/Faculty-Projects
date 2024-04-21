7ad93156fafe9ca016fb67e4c375d748d3fc16b0
Abdo <abdomoon100@gmail.com>
2024-04-21 14:38:25 +0200

    Add new 2 Projects :)

--- Intro to C#/Calculate Weight on Moon 3/Calculate Weight on Moon/Program.cs
+++ Intro to C#/Calculate Weight on Moon 3/Calculate Weight on Moon/Program.cs
@@ -6,8 +6,8 @@ public class Sheet
     {
         Console.WriteLine("To Calculate The Weight on Moon");
         Console.WriteLine("Write The your weight");
-        double weight = double.Parse(Console.ReadLine());
+        double weight = double.Parse(Console.ReadLine()!);
         double calc = weight*.17;
-        Console.WriteLine($"Your weight on moon is {calc}");
+        Console.WriteLine($"Your weight on moon is {calc} Kg");
     }
 }
---
+++ Intro to C#/The Sum of Series One/The Sum of Series One/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class SumSeriesOne
+{
+    public static void Main()
+    {
+        // the sum dosn't cross 50000 //
+        int sum = 0;
+        int n = 1;
+        while(sum < 50000)
+        {
+            sum += (int)Math.Pow(n,n);
+            // the n increases by 2 each time 
+            n += 2;
+            n++;
+        }
+        Console.WriteLine(sum);
+    }
+
+}
