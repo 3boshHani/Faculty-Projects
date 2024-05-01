966e1e888e583cba6ae8a167af99360c69bbd95a
Abdo <abdomoon100@gmail.com>
2024-05-01 19:01:11 +0300

    Add Two Projects About Matrices

---
+++ Intro to C#/Product Matrix/ProductMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class ProductMatrix
+{
+    public static void Main() {
+
+        // row in matrix one should equal coloum in matrix two to multiply //
+
+        int[,] s = {{2,4},{6,8}};
+        int[,] p = {{1,3},{5,7}};
+        int[,] result = new int[2,2];
+        // Matrix One
+        for (int i = 0;i<2;i++)
+        {
+            for (int j = 0;j<2;j++)
+            {
+                Console.Write(s[i, j] + " ");
+            } 
+            Console.WriteLine();   
+        } 
+        Console.WriteLine(" *");
+        // Matrix Two
+        for (int i = 0;i<2;i++)
+        {
+            for (int j = 0;j<2;j++)
+            {
+                Console.Write(p[i, j] + " ");
+            } 
+            Console.WriteLine();   
+        } 
+        Console.WriteLine(" =");
+        // the multiply
+        for (int i = 0;i<2;i++)
+        {
+            for (int j = 0;j<2;j++)
+            {
+                result[i,j] = s[i,j] * p[i,j];
+            }   
+        } 
+        // show result
+        for (int i = 0;i<2;i++)
+        {
+            for (int j = 0;j<2;j++)
+            {
+                Console.Write(result[i, j] + " ");
+            } 
+            Console.WriteLine();   
+        } 
+    }
+}
---
+++ Intro to C#/Read and Sum of Matrix/ReadSumMatrix/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class ReadSumMatrix
+{
+    public static void Main()
+    {
+
+        int[,] a = new int[2, 2];
+        int[,] b = new int[2, 2];
+        int[,] sum = new int[2, 2];
+        int[,] diff = new int[2, 2];
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                a[i,j] = int.Parse(Console.ReadLine()!);
+            }
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                b[i,j] = int.Parse(Console.ReadLine()!);
+            }
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                sum[i,j] = a[i,j] + b[i,j];
+            }
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                diff[i,j] = a[i,j] - b[i,j];
+            }
+        }
+        // to show the matrices
+        Console.WriteLine("Sum Of The Two Matrices:");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write(sum[i,j]+" ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Differance Of The Two Matrices:");
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write(diff[i,j]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
